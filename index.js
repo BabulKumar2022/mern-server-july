e6f54a8e73a47b8b33d4ee53d7646d540d2da59a
unknown <bb3583400@gmail.com>
2023-07-13 01:25:39 +0600

    curd

--- index.js
+++ index.js
@@ -1,37 +1,85 @@
 const express = require('express');
-const cors = require('cors')
+const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
-
-
-app.get('/', (req, res)=>{
-    res.send('Hello from server new')
-});
 
 app.use(cors());
 app.use(express.json());
 
-const users = [
-    {id:1, name: 'Kamal', email: 'kamal@gmail.com', phone: '017200000'},
-    {id:2, name: 'Jamal', email: 'jamal@gmail.com', phone: '018200000'},
-    {id:3, name: 'hamal', email: 'hamal@gmail.com', phone: '019200000'},
-    {id:4, name: 'Tamal', email: 'tamal@gmail.com', phone: '016200000'},
-]
-app.get('/users', (req, res)=>{
-    res.send(users)
-});
 
-app.post('/users', (req, res)=>{
-   console.log('POST API called')
-   const user =req.body;
-   user.id = users.length + 1;
-   users.push(user);
-   console.log(user)
-   res.send(user)
+// userName: bb3583400
+//password: qS8qaGsiWbYusKem
+
+
+const uri = "mongodb+srv://bb3583400:qS8qaGsiWbYusKem@cluster0.jpegwwj.mongodb.net/?retryWrites=true&w=majority";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
 });
 
 
-app.get('/user/:id', (req, res)=>{
+
+async function run() {
+  try {
+    await client.connect();
+    const userCollection = client.db('admin-db').collection('users');
+
+    console.log("db connected")
+
+app.get('/users', async (req, res)=>{
+  const query = {};
+  const cursor =userCollection.find(query);
+  const users = await cursor.toArray();
+  res.send(users)
+})
+
+app.post('/users', async (req, res)=>{
+        const newUser = req.body;
+        const result = await userCollection.insertOne(newUser);
+        console.log('adding new user', newUser);
+        res.send(result)
+});
+   
+} finally {
+    
+  }
+}
+run().catch(err =>console.log(err));
+
+
+//****************** */
+app.get('/', (req, res)=>{
+    res.send('Hello from server new')
+});
+ 
+
+// const users = [
+//     {id:1, name: 'Kamal', email: 'kamal@gmail.com', phone: '017200000'},
+//     {id:2, name: 'Jamal', email: 'jamal@gmail.com', phone: '018200000'},
+//     {id:3, name: 'hamal', email: 'hamal@gmail.com', phone: '019200000'},
+//     {id:4, name: 'Tamal', email: 'tamal@gmail.com', phone: '016200000'},
+// ]
+// app.get('/users', (req, res)=>{
+//     res.send(users)
+// });
+
+// app.post('/users', (req, res)=>{
+//    console.log('POST API called', req.body)
+//    const user = req.body;
+//    user.id = users.length + 1;
+//    users.push(user);
+//    console.log(user)
+//    res.send(user)
+// });
+
+
+app.get('/users/:id', (req, res)=>{
     console.log(req.params)
     const id = parseInt(req.params.id);
     const user = users.find(u => u.id == id)
